93b936600d40f988056177bb8d56160c2b03b832
Brian Love <brian@brianflove.com>
2018-01-09 18:48:27 -0700

    Fixed protractor app e2e test and adding Travis yaml.

--- client/e2e/app.po.ts
+++ client/e2e/app.po.ts
@@ -5,7 +5,7 @@ export class AppPage {
     return browser.get('/');
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  getRouterOutlet() {
+    return element(by.css('router-outlet'));
   }
 }
--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -34,12 +34,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  // it(`should have as title 'app'`, async(() => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.debugElement.componentInstance;
-  //   expect(app.title).toEqual('app');
-  // }));
-
   it('should render router-outlet', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
